b4f4fd83b0e80028e056e5a4a41999d753678936
tobirbd <tobias.rebenda@aon.at>
2026-01-22 13:14:55 +0100

    Added CheckWin method, First implements in Game class

--- 3d-vier-gewinnt/Assets/Scripts/Board.cs
+++ 3d-vier-gewinnt/Assets/Scripts/Board.cs
@@ -1,8 +1,9 @@
+using System.Drawing;
 using UnityEngine;
 
 public class Board
 {
-    private Player[,,] grid;
+    public Player[,,] grid;
     public const int Size = 4;
 
     public Board()
@@ -44,4 +45,38 @@ public class Board
             y >= 0 && y < Size &&
             z >= 0 && z < Size;
     }
+    public bool CheckWin(Player player)
+    {
+        int[,] directions = new int[,]
+        {
+            { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 },
+            { 1, 1, 0 }, { 1, 0, 1 }, { 0, 1, 1 },
+            { 1, 1, 1 }, { 1, -1, 1 }, { -1, 1, 1 }, { 1, 1, -1 }
+        };
+
+        for (int x = 0; x < Size; x++)
+        {
+            for (int y = 0; y < Size; y++)
+            {
+                for (int z = 0; z < Size; z++)
+                {
+                    if (grid[x, y, z] != player)
+                    {
+                        continue;
+                    }
+
+                    for (int d = 0;  d < directions.GetLength(0); d++)
+                    {
+                        
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    private bool HasFour(int x, int y, int z, int dx, int dy, int dz)
+    {
+        return true;
+    }
+
 }
--- 3d-vier-gewinnt/Assets/Scripts/Game.cs
+++ 3d-vier-gewinnt/Assets/Scripts/Game.cs
@@ -4,12 +4,39 @@ public class Game : MonoBehaviour
 {
     private Board board;
     private Player currentPlayer = Player.Player1;
-    
+
     void Start()
     {
         board = new Board();
         Debug.Log("Spiel gestartet. Spieler 1 beginnt.");
     }
-    
-    
+
+    public void TryMakeMove(int x, int y, int z)
+    {
+        bool success = board.PlacePiece(x, y, z, currentPlayer);
+
+        if (!success)
+        {
+            Debug.Log("Ungültiger Spielzug!");
+            return;
+        }
+
+        Debug.Log($"Spielzug: {currentPlayer} -> {x},{y},{z}");
+
+        CheckWin();
+
+        SwitchPlayer();
+    }
+
+    private void SwitchPlayer()
+    {
+        currentPlayer = currentPlayer == Player.Player1 ? Player.Player2 : Player.Player1;
+
+        Debug.Log($"{currentPlayer} ist an der Reihe!");
+    }
+
+    private bool CheckWin()
+    {
+        return true;
+    }
 }
