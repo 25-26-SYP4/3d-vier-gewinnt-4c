7d64c667bf450bffa7210a3cbc7f63cf618c88a5
tobirbd <tobias.rebenda@aon.at>
2026-02-25 11:36:52 +0100

    show-current-player done

--- 3d-vier-gewinnt/Assets/Scripts/Game.cs
+++ 3d-vier-gewinnt/Assets/Scripts/Game.cs
@@ -1,22 +1,28 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game : MonoBehaviour
 {
     public Board board;
     public Player currentPlayer = Player.Player1;
+    public Image player1Panel;
+    public Image player2Panel;
 
+    public TextMeshProUGUI player1Text;
+    public TextMeshProUGUI player2Text;
     public bool gameOver = false;
 
     void Start()
     {
         board = new Board();
-        Debug.Log("Spiel gestartet. Spieler 1 beginnt.");
+        UpdatePlayerUI();
     }
 
     public void TryMakeMove(int x, int y, int z)
     {
         if (gameOver) return;
-        
+
         Debug.Log("TryMakeMove aufgerufen");
         bool success = board.PlacePiece(x, y, z, currentPlayer);
 
@@ -46,8 +52,34 @@ public class Game : MonoBehaviour
     {
         currentPlayer = currentPlayer == Player.Player1 ? Player.Player2 : Player.Player1;
 
-        Debug.Log($"{currentPlayer} ist an der Reihe!");
+        UpdatePlayerUI();
     }
-
-    
+    public void UpdatePlayerUI()
+    {
+        if (currentPlayer == Player.Player1)
+        {
+            SetActivePlayer(player1Panel, player1Text, true);
+            SetActivePlayer(player2Panel, player2Text, false);
+        }
+        else
+        {
+            SetActivePlayer(player1Panel, player1Text, false);
+            SetActivePlayer(player2Panel, player2Text, true);
+        }
+    }
+    void SetActivePlayer(Image panel, TextMeshProUGUI text, bool active)
+    {
+        if (active)
+        {
+            panel.color = new Color(55f / 255, 55f / 255, 55f / 255, 100f);
+            text.color = Color.white;
+            panel.transform.localScale = Vector3.one * 1.1f;
+        }
+        else
+        {
+            panel.color = new Color(55f/255, 55f/255, 55f / 255, 0.3f);
+            text.color = Color.gray;
+            panel.transform.localScale = Vector3.one;
+        }
+    }
 }
